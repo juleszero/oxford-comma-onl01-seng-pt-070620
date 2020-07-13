71e6bc5ded921851e7c6419992b8e92e6de8c9ee
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-13 22:10:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,18 +3,14 @@ def oxford_comma(array)
     when 1
       array.join()
     when 1..2
-    array.join(' and ')
+      array.join(' and ')
   else
     ox_comma = []
     array.each do |comma|
     ox_comma << comma+","
-    ox_comma.insert(-2, " and ").join(" ")
+    ox_comma.insert(-2, " and ")
+    ox_comma.join(" ")
 end
 end
 
 
-fruits.each do |fruit|
-  new_fruit << fruit+","
-end
-new_fruit.insert(-2, " and ")
-puts new_fruit.join(" ")
