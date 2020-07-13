735d6963a065adab33f4ef4f96dc0175c2d82145
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-13 21:48:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,13 +5,5 @@ def oxford_comma(array)
       array.join()
     when 1..2
     array.join(' and ')
-  
-  if array.length == 2
-    return array.join(" and ")
-  elsif array.length >= 3
-    end
-  
-else
-  return array.join
 end
 end
